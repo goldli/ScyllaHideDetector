d4b7b46980851f64dfadae5d986c109883ce7acc
lurumdare <8020186+lurumdare@users.noreply.github.com>
2019-11-04 14:37:20 +0300

    Update code

--- Source.cpp
+++ Source.cpp
@@ -18,14 +18,16 @@ void ntdll_restore(const char *func_name)
   const auto ntdll = GET_MODULE_BASE_ADDRESS(L"ntdll.dll");
   PVOID ntdll_mapped = nullptr;
   MAP_NATIVE_MODULE("ntdll.dll", &ntdll_mapped);
-  const auto hooked_func_adress = resolve_jmp(get_proc_address(ntdll, HASHSTR(func_name)), 1);
-  const auto hooked_func_size = static_cast<size_t>(get_size_of_proc(hooked_func_adress, 1));
-  const auto crc_hooked = crc32(hooked_func_adress, static_cast<unsigned int>(hooked_func_size));
+  // original
   const auto original_func_adress = resolve_jmp(get_proc_address(ntdll_mapped, HASHSTR(func_name)), 1);
   const auto original_func_size = static_cast<size_t>(get_size_of_proc(original_func_adress, 1));
-  const auto crc_original = crc32(original_func_adress, static_cast<unsigned int>(original_func_size));
+  const auto _original = RtlCompareMemory(original_func_adress, original_func_adress, original_func_size);
+  // hooked
+  const auto hooked_func_adress = resolve_jmp(get_proc_address(ntdll, HASHSTR(func_name)), 1);
+  const auto hooked_func_size = static_cast<size_t>(get_size_of_proc(hooked_func_adress, 1));
+  const auto _hooked = RtlCompareMemory(hooked_func_adress, hooked_func_adress, hooked_func_size);
   // detect hook and restore bytes
-  if (crc_original != crc_hooked)
+  if (_original != _hooked)
   {
 #ifndef AHOOK_LOG
     log("[Detect] " + static_cast<std::string>(func_name) + "\r\n");
@@ -48,14 +50,16 @@ void kernelbase_restore(const char *func_name)
   const auto kernelbase = GET_MODULE_BASE_ADDRESS("kernelbase.dll");
   PVOID kernelbase_mapped = nullptr;
   MAP_NATIVE_MODULE("kernelbase.dll", &kernelbase_mapped);
-  const auto hooked_func_adress = resolve_jmp(get_proc_address(kernelbase, HASHSTR(func_name)), 1);
-  const auto hooked_func_size = static_cast<size_t>(get_size_of_proc(hooked_func_adress, 1));
-  const auto crc_hooked = crc32(hooked_func_adress, static_cast<unsigned int>(hooked_func_size));
+  // original
   const auto original_func_adress = resolve_jmp(get_proc_address(kernelbase_mapped, HASHSTR(func_name)), 1);
   const auto original_func_size = static_cast<size_t>(get_size_of_proc(original_func_adress, 1));
-  const auto crc_original = crc32(original_func_adress, static_cast<unsigned int>(original_func_size));
+  const auto _original = RtlCompareMemory(original_func_adress, original_func_adress, original_func_size);
+  // hooked
+  const auto hooked_func_adress = resolve_jmp(get_proc_address(kernelbase, HASHSTR(func_name)), 1);
+  const auto hooked_func_size = static_cast<size_t>(get_size_of_proc(hooked_func_adress, 1));
+  const auto _hooked = RtlCompareMemory(hooked_func_adress, hooked_func_adress, hooked_func_size);
   // detect hook and restore bytes
-  if (crc_original != crc_hooked)
+  if (_original != _hooked)
   {
 #ifndef AHOOK_LOG
     log("[Detect] " + static_cast<std::string>(func_name) + "\r\n");
@@ -82,14 +86,16 @@ void user32_restore(const char *func_name)
     const auto win32_u = GET_MODULE_BASE_ADDRESS("win32u.dll");
     PVOID win32_u_mapped = nullptr;
     MAP_NATIVE_MODULE("win32u.dll", &win32_u_mapped);
-    const auto hooked_func_adress = resolve_jmp(get_proc_address(win32_u, HASHSTR(func_name)), 1);
-    const auto hooked_func_size = static_cast<size_t>(get_size_of_proc(hooked_func_adress, 1));
-    const auto crc_hooked = crc32(hooked_func_adress, static_cast<unsigned int>(hooked_func_size));
+    // original
     const auto original_func_adress = resolve_jmp(get_proc_address(win32_u_mapped, HASHSTR(func_name)), 1);
     const auto original_func_size = static_cast<size_t>(get_size_of_proc(original_func_adress, 1));
-    const auto crc_original = crc32(original_func_adress, static_cast<unsigned int>(original_func_size));
+    const auto _original = RtlCompareMemory(original_func_adress, original_func_adress, original_func_size);
+    // hooked
+    const auto hooked_func_adress = resolve_jmp(get_proc_address(win32_u, HASHSTR(func_name)), 1);
+    const auto hooked_func_size = static_cast<size_t>(get_size_of_proc(hooked_func_adress, 1));
+    const auto _hooked = RtlCompareMemory(hooked_func_adress, hooked_func_adress, hooked_func_size);
     // detect hook and restore bytes
-    if (crc_original != crc_hooked)
+    if (_original != _hooked)
     {
 #ifndef AHOOK_LOG
       log("[Detect] " + static_cast<std::string>(func_name) + "\r\n");
@@ -113,14 +119,16 @@ void user32_restore(const char *func_name)
     const auto user_32 = GET_MODULE_BASE_ADDRESS(L"user32.dll");
     PVOID user32_mapped = nullptr;
     MAP_NATIVE_MODULE("user32.dll", &user32_mapped);
-    const auto hooked_func_adress = resolve_jmp(get_proc_address(user_32, HASHSTR(func_name)), 1);
-    const auto hooked_func_size = static_cast<size_t>(get_size_of_proc(hooked_func_adress, 1));
-    const auto crc_hooked = crc32(hooked_func_adress, static_cast<unsigned int>(hooked_func_size));
+    // original
     const auto original_func_adress = resolve_jmp(get_proc_address(user32_mapped, HASHSTR(func_name)), 1);
     const auto original_func_size = static_cast<size_t>(get_size_of_proc(original_func_adress, 1));
-    const auto crc_original = crc32(original_func_adress, static_cast<unsigned int>(original_func_size));
+    const auto _original = RtlCompareMemory(original_func_adress, original_func_adress, original_func_size);
+    // hooked
+    const auto hooked_func_adress = resolve_jmp(get_proc_address(user_32, HASHSTR(func_name)), 1);
+    const auto hooked_func_size = static_cast<size_t>(get_size_of_proc(hooked_func_adress, 1));
+    const auto _hooked = RtlCompareMemory(hooked_func_adress, hooked_func_adress, hooked_func_size);
     // detect hook and restore bytes
-    if (crc_original != crc_hooked)
+    if (_original != _hooked)
     {
 #ifndef AHOOK_LOG
       log("[Detect] " + static_cast<std::string>(func_name) + "\r\n");
